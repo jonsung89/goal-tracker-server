c97924f4f84c357f98283dcabedb2aa79c2e75a6
Jon Sung <jonsung89@gmail.com>
2019-02-10 21:58:15 -0600

    cors config fix

--- config.js
+++ config.js
@@ -2,9 +2,11 @@
 
 require('dotenv').config();
 
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/goal-tracker-server';
-exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/goal-tracker';
-exports.PORT = process.env.PORT || 8080;
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+module.exports = {
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/goal-tracker-server',
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/goal-tracker',
+  PORT: process.env.PORT || 8080,
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
+}
